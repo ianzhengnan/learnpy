f5e09405286e5da0f8076979984919090b09a6d7
Ian Zheng <ian.zheng@sap.com>
2016-03-25 17:32:55 +0800

    add udp

---
+++ udp_client.py
@@ -0,0 +1,10 @@
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+for data in [b'Ian', b'flks', b'KaKa']:
+    s.sendto(data, ('127.0.0.1', 9999))
+    print(s.recv(1024).decode('utf-8'))
+
+s.close()
+
