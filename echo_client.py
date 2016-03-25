f9cebd2aae96840f9fac7e8c54c646426e921140
Ian Zheng <ian.zheng@sap.com>
2016-03-25 17:13:20 +0800

    correct echo_client.py

--- echo_client.py
+++ echo_client.py
@@ -5,7 +5,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect(('127.0.0.1', 9999))
 
-print(s.secv(1024).decode('utf-8'))
+print(s.recv(1024).decode('utf-8'))
 for data in [b'Ian', b'flks', b'KaKa']:
     s.send(data)
     print(s.recv(1024).decode('utf-8'))
