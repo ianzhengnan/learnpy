a0759f4358b5f7631d8d4291dc73fc6a3af6ac2a
Ian Zheng <ian.zheng@sap.com>
2016-03-31 10:30:11 +0800

    update mysql_ext

--- mysql_ext.py
+++ mysql_ext.py
@@ -1,14 +1,14 @@
 
 import mysql.connector
 
-conn = mysql.connector.connect(user='root', password='', database='test')
+conn = mysql.connector.connect(user='root', password='1234567', database='test')
 
 cursor = conn.cursor()
 
-cursor.execute(r'create table user(id varchar(20) primary key, name varchar(20))')
+# cursor.execute(r'create table user(id varchar(20) primary key, name varchar(20))')
 
-cursor.execute('insert into user (id, name) values (%s, %s)', ['1', 'Ian'])
-cursor.execute('insert into user (id, name) values (%s, %s)', ['2', 'flks'])
+cursor.execute('insert into user (id, name) values (%s, %s)', ['5', 'Ian'])
+cursor.execute('insert into user (id, name) values (%s, %s)', ['6', 'flks'])
 
 conn.commit()
 cursor.close()
